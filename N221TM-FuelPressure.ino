28f619434d4602d4b217c8397fa7987dcb3f026b
Mike Rehberg <Mike@TR182.com>
2016-04-07 09:41:33 -0400

    Added 9600 to 4800 BAUD rate converter

--- N221TM-FuelPressure.ino
+++ N221TM-FuelPressure.ino
@@ -12,11 +12,17 @@
  
  created 29 Dec. 2008
  modified 9 Apr 2012 by Tom Igoe
- modified for N221TM in January 2016 by Mike Rehberg
+ modified Januauary 2016 by Mike Rehberg: For N221TM Fuel Pressure transducer ouputs 0.5Vdc to 4.5Vdc
+                                        while VM1000 is expecting 0 to 50mv.
+ modiified March 2016 by Mike Rehberg:  Added Serial BAUD rate converter.  Garmin G496 outputs NMEA
+                                        and Garmin COM data at 9600 BAUD.  Trio AP-1 autopilot can
+                                        only read NMEA-0183 data at 4800 BAUD.
  */
 
 // These constants won't change.  They're used to give names
 // to the pins used:
+// Note Pin-0 is hardware RX and Pin-1 is hardware TX.  The RX Pin-0 will be connected to the G496 TX
+
 const int analogInPin = A0;  // Analog input pin that the Fuel Sensor is attached to
 const int analogOutPin = 3;  // Analog output pin that is voltage divided before going to
 float psig;                  // The VM-1000
@@ -25,11 +31,16 @@ float temp;
 float resolution=206.25;    // Input steps per Volt  1023/4.96
 float scaleFactor=17.0;     // Output steps per PSIG: 255/15
 int   garminOffset=102;     // Steps for 0.5 VDC offset to 0.0 PSIG
+char  incomingByte;         // BAUD conversion buffer
 
-#define rxPin 2    //  rxPin is immaterial - not used - just make this an unused Arduino pin number
-#define txPin 15   //  txpin for Serial 4x20 LCD Display using Analog(1)
+#define rxPinLCD 2    //  rxPinLCD is immaterial - not used - just make this an unused Arduino pin number
+#define txPinLCD 15   //  txpinLCD for Serial 4x20 LCD Display using Analog(1)
 
-SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
+#define rxPinAP1 3    //  rxPinAP1 is immaterial - not used - just make this an unused Arduino pin number
+#define txPinAP1 16   //  txpinAP1 for Serial 4x20 LCD Display using Analog(2)
+
+SoftwareSerial mySerial =  SoftwareSerial(rxPinLCD, txPinLCD);
+SoftwareSerial ap1Serial =  SoftwareSerial(rxPinAP1, txPinAP1);
 
 int rawSensorValue = 0;     // value read from the Garmin Sensor
 int sensorValue;
@@ -39,10 +50,14 @@ void setup() {
   pinMode(analogOutPin, OUTPUT);
   
   // initialize serial communications at 9600 bps:
-  Serial.begin(9600);       // Diagnostics serial to PC running Arduino application
+  Serial.begin(9600);       // Diagnostics serial to PC running Arduino application and G496 NMEA/COM
 
-  pinMode(txPin, OUTPUT);   // Serial output to 4/20 character LCD display
+  pinMode(txPinLCD, OUTPUT); // Serial output to 4/20 character LCD display
   mySerial.begin(9600);      // 9600 baud is chip comm speed
+
+  pinMode(txPinAP1, OUTPUT); // Serial output to NavAid AP-1 Autopilot
+  ap1Serial.begin(4800);      // 4800 baud is chip comm speed
+  
   mySerial.print("?G420");   // set display geometry,  4 x 20 characters in this case
   delay(500);                // pause to allow LCD EEPROM to program
   mySerial.print("?Bff");    // set backlight to ff hex, maximum brightness
@@ -61,6 +76,12 @@ void setup() {
 }
 
 void loop() {
+
+  if(Serial.available() > 0) {
+    incomingByte = Serial.read();
+    ap1Serial.print(incomingByte);
+  }
+
   // read the analog in value:
   rawSensorValue = analogRead(analogInPin);
   sensorValue = rawSensorValue - garminOffset;  // Remove the 0.5V offset
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Fuel-Pressure-N221TM
 Voltage converter to match Garmin G3X Fuel Pressure sensor to VM-1000 engine monitor
+
+Serial buffer and BAUD rate converter for G496 (9600) to Smart-GPS (4800).
+
